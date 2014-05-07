26b9dd573d3a08d155d4f5c3b36e3fb1b304256f
Jacob McIntosh <nacitar@ubercpp.com>
2014-05-07 14:50:23 -0500

    Now building with access to nx-core library

--- common_builds.sh
+++ common_builds.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+cd "$(dirname "$0")"
+
+root_dir="$PWD"
+cmake_repo_dir="$root_dir/external/cmake"
+
 [ -d 'build' ] && rm -rf build
-mkdir build || exit 1
-pushd build && cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/avr_toolchain.cmake .. && popd
+mkdir -p build/avr || exit 1
+
+
+pushd build/avr &&
+  cmake -DCMAKE_TOOLCHAIN_FILE=$cmake_repo_dir/avr_toolchain.cmake $root_dir &&
+  popd
